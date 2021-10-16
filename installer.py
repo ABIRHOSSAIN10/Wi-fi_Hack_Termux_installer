29c305bfdb34724ea741f8d112780c2ea0cd0877
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-10-16 17:08:36 +0600

    Update installer.py

--- installer.py
+++ installer.py
@@ -3,8 +3,8 @@ print("you need to root your phone")
 print()
 os.system("pkg install -y root-repo")
 os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
-os.system("cd ..;git clone https://github.com/ABIRHOSSAIN10/OneShot_Termux_installer")
+os.system("cd ..;git clone https://github.com/ABIRHOSSAIN10/WIFI-HACK")
 
-os.system("cd ..;chmod +x OneShot/oneshot.py")
+os.system("cd ..;chmod +x WIFI-HACK/oneshot.py")
 
-print("Enter This Command :\nsudo python OneShot/oneshot.py -i wlan0 -K")
+print("Enter This Command :\nsudo python WIFI-HACK/oneshot.py -i wlan0 -K")
