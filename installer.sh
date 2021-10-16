5a7350b06779d0edcccb8bded78e4dd6a4d0cc9c
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-10-16 16:30:07 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 pkg install -y root-repo 
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone --depth 1 https://github.com/drygdryg/OneShot OneShot
+git clone --depth 1 https://github.com/ABIRHOSSAIN10/OneShot OneShot
 
 chmod +x OneShot/oneshot.py
 
